c6cf00c31802be1375a52a8bcc350fae259e4046
stella <khk9562@naver.com>
2024-06-29 23:10:02 +0900

    design: section3에도 아이템 추가

--- src/App.js
+++ src/App.js
@@ -93,6 +93,11 @@ function App() {
       <Section background="https://plus.unsplash.com/premium_photo-1661379066665-9510f616f349?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
         <div>
           <h2>Splash into Excitement at Sunway Waterpark</h2>
+          <p>
+            Immerse yourself in the beauty of nature without sacrificing
+            comfort. Our luxury glamping sites offer a perfect blend of outdoor
+            adventure and modern amenities under the starry sky.
+          </p>
           <div
             className={`list max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none`}
           >
@@ -130,6 +135,30 @@ function App() {
             comfort. Our luxury glamping sites offer a perfect blend of outdoor
             adventure and modern amenities under the starry sky.
           </p>
+          <div
+            className={`list max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none`}
+          >
+            <FadeInWhenVisible>
+              <div className="flex flex-col h-full p-6 bg-gray-800">
+                {/* Content of the first item */}
+                dfsdfds
+              </div>
+            </FadeInWhenVisible>
+
+            <FadeInWhenVisible delay={0.2}>
+              <div className="flex flex-col h-full p-6 bg-gray-800">
+                {/* Content of the second item */}
+                dfsdfdsf
+              </div>
+            </FadeInWhenVisible>
+
+            <FadeInWhenVisible delay={0.4}>
+              <div className="flex flex-col h-full p-6 bg-gray-800">
+                {/* Content of the third item */}
+                dfsfds
+              </div>
+            </FadeInWhenVisible>
+          </div>
         </div>
       </Section>
     </div>
